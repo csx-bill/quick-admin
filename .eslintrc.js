646e404985441430c9a2314f7bdc5ea59d07bc39
Bill <1594805355@qq.com>
2023-04-07 23:02:54 +0800

    feat(compiler): 切换 cxd 样式

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,9 @@ module.exports = {
     page: true,
     REACT_APP_ENV: true,
   },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['off'],
+    'no-param-reassign': ['off'],
+    '@typescript-eslint/no-unused-expressions': ['off'],
+  },
 };
