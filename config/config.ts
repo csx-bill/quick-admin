b43d537cd37e0cbf9be8210b7a3142e955a8aa30
Bill <1594805355@qq.com>
2023-06-04 20:11:40 +0800

    feat(compiler):  esbuildMinifyIIFE: true,

--- config/config.ts
+++ config/config.ts
@@ -8,6 +8,7 @@ import routes from './routes';
 const { REACT_APP_ENV = 'dev' } = process.env;
 
 export default defineConfig({
+  esbuildMinifyIIFE: true,
   /**
    * @name 开启 hash 模式
    * @description 让 build 之后的产物包含 hash 后缀。通常用于增量发布和避免浏览器加载缓存。
