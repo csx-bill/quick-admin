2dc9b988fac7cee623717d9ed77fc02049e291dd
Bill <1594805355@qq.com>
2023-10-17 11:01:52 +0800

    清空 旧代码

--- src/locales/en-US/tabs.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    'tabs.close.left': 'Close the left tab',
-    'tabs.close.right': 'Close the right tab',
-    'tabs.close.others': 'Close other tabs',
-    'tabs.refresh': 'Refresh',
-  };
