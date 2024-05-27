0116324e4f7b1ba20c5d84a937b03a9444b6c441
Bill <1594805355@qq.com>
2024-05-27 18:04:40 +0800

    fix: 访问标题显示问题

--- src/hooks/web/useTitle.ts
+++ src/hooks/web/useTitle.ts
@@ -3,14 +3,17 @@ import { useLocation } from 'react-router-dom'
 import { useTitle as usePageTitle } from 'ahooks'
 import { searchRoute } from '@/utils'
 import { basicRoutes } from '@/router'
+import { useAppSelector } from '@/stores'
 
 // 监听页面变化和动态改变网站标题
 export function useTitle() {
   const [pageTitle, setPageTitle] = useState('react-admin-design')
   const { pathname } = useLocation()
+  const { tenantRouter } = useAppSelector(state => state.tenantRouter)
 
   useEffect(() => {
-    const currRoute = searchRoute(pathname, basicRoutes)
+    const routes = [...basicRoutes, ...tenantRouter]
+    const currRoute = searchRoute(pathname, routes)
     setPageTitle(currRoute?.meta.title)
   }, [pathname])
 
