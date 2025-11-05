e06223b1498f27659f73162c608a7430ccef5a83
Bill <1594805355@qq.com>
2025-11-05 12:12:23 +0800

    chore：pnpm workspace 统一依赖版本管理

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -7,18 +7,8 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      'i18n-runtime': path.resolve(__dirname, './node_modules/i18n-runtime/lib/index.js'),
     }
   },
-  optimizeDeps: {
-    include: [
-        `monaco-editor/esm/vs/language/json/json.worker`,
-        `monaco-editor/esm/vs/language/css/css.worker`,
-        `monaco-editor/esm/vs/language/html/html.worker`,
-        `monaco-editor/esm/vs/language/typescript/ts.worker`,
-        `monaco-editor/esm/vs/editor/editor.worker`
-    ],
-},
   server: {
     proxy: {
       // 代理所有以 /api 开头的请求
